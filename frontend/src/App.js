f054853560ce465ba6271fa0fa925cb9baf2d591
Manasa G <manasa.g7890@hotmail.com>
2023-10-18 21:28:24 +0530

    feth files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+
+
+
+  React.useEffect(()=>{
+    axios.get('http://localhost:5555/books/').then((response)=> console.log(response))
+  },[])
   return (
     <div className="App">
      <h1>Test with CORS policy</h1>
+
     </div>
   );
 }
