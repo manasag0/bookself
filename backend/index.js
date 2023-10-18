c7747a858863253e81f6d5584041d5b1584648df
Manasa G <manasa.g7890@hotmail.com>
2023-10-18 21:34:39 +0530

    file fetched

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,21 @@ import express, { response } from "express";
 import  {PORT , mongoDBURL} from "./config.js";
 import mongoose from "mongoose";
 import bookRoute from "./bookRoutes/bookRoute.js";
+import cors from 'cors';
+
 const app = express();
   
 app.use(express.json());
+
+app.use(cors())
+
+app.use(cors({
+    origin:"http://localhost:3000",
+    methods: ['GET','POST','PUT','DELETE'],
+    allowedHeaders: ['Content-type']
+
+}));
+
 app.use("/books",bookRoute)
 
 app.get('/',(req,res)=>{
