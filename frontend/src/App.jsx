5bde381ffae00db0f50ac0a0678cac0c9dfac9fa
Manasa G <manasa.g7890@hotmail.com>
2023-10-18 22:29:21 +0530

    files fetched

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+
+const App = () => {
+  return (
+    <div className='bg-red-400 text-white'>
+      Hello vite
+    </div>
+  )
+}
+
+export default App
