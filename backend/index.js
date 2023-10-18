e1b9c22310fa1288f1cee3a76e7c68792fb3fdf9
Manasa G <manasa.g7890@hotmail.com>
2023-10-18 20:14:11 +0530

    feth back files

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,30 @@
-import express from "express";
-import  {PORT} from "./config.js";
+import express, { response } from "express";
+import  {PORT , mongoDBURL} from "./config.js";
+import mongoose from "mongoose";
+import { Book } from "./Models/bookModel.js";
+import bookRoute from "./bookRoutes/bookRoute.js";
 const app = express();
+
+app.use(express.json());
+app.use("/books",bookRoute)
+
+app.get('/',(req,res)=>{
+    console.log(req)
+    return res.status(200).send("Welcome to MERN stack lession")
+})
+
+
+mongoose.connect(mongoDBURL)
+.then(()=>{
+    console.log('Connected to MongoDB');
+})
+.catch((error)=>{
+    console.log('Error connecting to MongoDB: ', error);
+})
+
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
 })
+
+
