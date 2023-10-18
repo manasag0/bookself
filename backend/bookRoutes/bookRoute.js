829c42876ad2aa15048a5abed0efe7c522c94a60
Manasa G <manasa.g7890@hotmail.com>
2023-10-18 20:53:03 +0530

    file fetched

--- backend/bookRoutes/bookRoute.js
+++ backend/bookRoutes/bookRoute.js
@@ -19,13 +19,31 @@ router.get("/",async(req,res)=>{
         })
     }
 })
+router.get("/search/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const book = await Book.findById(id);
+  
+      if (book) {
+        return res.status(200).json(book);
+      } else {
+        return res.status(404).json({ message: "Book not found with id" });
+      }
+    } catch (error) {
+      console.error(error.message);
+      return res.status(500).json({ message: error.message });
+    }
+  });
+  
+
 router.get("/:id",async(req,res)=>{
     try{
         const {id} = req.params;
         const book = await Book.findById(
        id)
         
-        return res.status(200).send(book)
+
+
     }catch(error){
         console.log(error.message);
         res.status(500).send({
