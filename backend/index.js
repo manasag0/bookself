e06321bb3a47bed14bc25b492e7d3224feb17986
Manasa G <manasa.g7890@hotmail.com>
2023-10-19 19:23:01 +0530

    files feth

--- backend/index.js
+++ backend/index.js
@@ -8,14 +8,14 @@ const app = express();
   
 app.use(express.json());
 
-app.use(cors())
+// app.use(cors())
 
-app.use(cors({
-    origin:"http://localhost:3000",
-    methods: ['GET','POST','PUT','DELETE'],
-    allowedHeaders: ['Content-type']
+// app.use(cors({
+//     origin:"http://localhost:3000",
+//     methods: ['GET','POST','PUT','DELETE'],
+//     allowedHeaders: ['Content-type']
 
-}));
+// }));
 
 app.use("/books",bookRoute)
 
